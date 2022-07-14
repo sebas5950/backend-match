bfec89ea6a66b01f5c86d76e6e9c0d1fe333d023
Alex Guanipatin <sebas5950@gmail.com>
2022-07-14 17:22:16 -0400

    herokw

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,10 @@
 # This is an _environment variable_ that is used by some of the Rake tasks to determine
 # if our application is running locally in development, in a test environment, or in production
-# ENV['RACK_ENV'] ||= "development"
+ENV['RACK_ENV'] ||= "development"
 
 # Require in Gems
-# require 'bundler/setup'
-# Bundler.require(:default, ENV['RACK_ENV'])
+require 'bundler/setup'
+Bundler.require(:default, ENV['RACK_ENV'])
 
 # Require in all files in 'app' directory
 configure :development do
